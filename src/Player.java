25d7f42c94e1650c4461bef27377d55f306b33f4
Shira <s.yosov@student.tue.nl>
2025-10-14 17:10:38 +0200

    fiexes

--- src/Player.java
+++ src/Player.java
@@ -1,18 +1,55 @@
-import java.awt.Image;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 abstract class Player {
 
     protected FoodItem dish;
-    protected Image playerImage;
+    protected BufferedImage playerImage;
 
     protected int playerX;
     protected int playerY;
 
-    public Player() {
-        
+    public Player(int x, int y) {
+
+        this.playerX = x;
+        this.playerY = y;
         this.dish = null;
-        this.playerX = 100;
-        this.playerY = 100;
+        
+        try {
+            BufferedImage original = ImageIO.read(getClass().getResource("/pictures/waitress.png"));
+
+            Image tmp = original.getScaledInstance(70, 70, Image.SCALE_SMOOTH); // scale image
+            this.playerImage = new BufferedImage(70, 70, BufferedImage.TYPE_INT_ARGB);
+
+            Graphics2D g2 = this.playerImage.createGraphics();
+            g2.drawImage(tmp, 0, 0, null);
+            g2.dispose();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.playerImage = null; // fallback to circle
+        }
+    }
+
+    public void setPosition(int x, int y) {
+        this.playerX = x;
+        this.playerY = y;
+    }
+
+    public int getX() {
+        return this.playerX;
+    }
+
+    public int getY() {
+        return this.playerY;
+    }
+
+    public void drawWaitress(Graphics g) {
+        if (this.playerImage != null) {
+            g.drawImage(this.playerImage, this.playerX - this.playerImage.getWidth() / 2,
+                    this.playerY - this.playerImage.getHeight() / 2, null);
+        }
     }
 
     public void setDish(FoodItem item) {
@@ -23,7 +60,7 @@ abstract class Player {
         return this.dish;
     }
 
-    public void setPlayerImage(Image image) {
+    public void setPlayerImage(BufferedImage image) {
         this.playerImage = image;
     }
 
--- src/Waitress.java
+++ src/Waitress.java
@@ -2,45 +2,16 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
-public class Waitress {
-    private int x;
-    private int y;
-    private BufferedImage image;
+public class Waitress extends Player {
 
     public Waitress(int startX, int startY) {
-        x = startX;
-        y = startY;
 
-        try {
-            BufferedImage original = ImageIO.read(getClass().getResource("/pictures/waitress.png"));
+        super(startX, startY);
+        
+        // x = startX;
+        // y = startY;
 
-            Image tmp = original.getScaledInstance(70, 70, Image.SCALE_SMOOTH); //scale image
-            image = new BufferedImage(70, 70, BufferedImage.TYPE_INT_ARGB);
-
-            Graphics2D g2 = image.createGraphics();
-            g2.drawImage(tmp, 0, 0, null);
-            g2.dispose();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            image = null; // fallback to circle
-        }
     }
 
-    public void setPosition(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() { return x; }
-    public int getY() { return y; }
-
-    public void drawWaitress(Graphics g) {
-        if (image != null) {
-            g.drawImage(image, x - image.getWidth()/2, y - image.getHeight()/2, null);
-        } else {
-            g.setColor(Color.RED);
-            g.fillOval(x - 10, y - 10, 20, 20);
-        }
-    }
+    
 }
